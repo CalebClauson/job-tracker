28280050242f82ec1d14c2f6081951fc34a6a9f5
CalebClauson <calebjr.2@gmail.com>
2026-03-31 01:39:29 -0400

    Commented original database creation

--- db.py
+++ db.py
@@ -3,17 +3,18 @@ import sqlite3
 conn = sqlite3.connect("jobs.db")
 cursor = conn.cursor()
 
-cursor.execute("""
-           CREATE TABLE jobs(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            company TEXT,
-            title TEXT,
-            status TEXT,
-            date_applied TEXT,
-            location TEXT,
-            notes TEXT,
-            link TEXT
-           )""")
+#Initial Database created
+# cursor.execute("""
+#            CREATE TABLE jobs(
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             company TEXT,
+#             title TEXT,
+#             status TEXT,
+#             date_applied TEXT,
+#             location TEXT,
+#             notes TEXT,
+#             link TEXT
+#            )""")
 
 conn.commit()
 conn.close()
