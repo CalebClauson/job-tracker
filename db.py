568274239b6b4cfca2b0ee342d16e298e2810944
CalebClauson <calebjr.2@gmail.com>
2026-03-31 01:33:54 -0400

    Set up initial job tracker database framework

---
+++ db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("jobs.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+           CREATE TABLE jobs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            company TEXT,
+            title TEXT,
+            status TEXT,
+            date_applied TEXT,
+            location TEXT,
+            notes TEXT,
+            link TEXT
+           )""")
+
+conn.commit()
+conn.close()
