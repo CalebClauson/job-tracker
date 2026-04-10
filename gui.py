6a33fc83de077b7447058575e55d7289640237ed
CalebClauson <calebjr.2@gmail.com>
2026-04-10 09:26:56 -0400

    Clean up Tkinter layout, center form states, and guard edit selection

--- gui.py
+++ gui.py
@@ -14,7 +14,10 @@ FG_TEXT = "#2F241C"
 def start_gui():
     root = tk.Tk()
     root.title("Job-Tracker")
-    root.geometry("800x600")    
+    root.geometry("800x600")
+    root.resizable(False, False)
+    root.maxsize(800, 600)
+    root.minsize(800, 600)
     root.configure(bg=BG_MAIN)
     state = "main"
 
@@ -36,19 +39,16 @@ def start_gui():
 
     #frame definition
     top_frame = tk.Frame(root, bg=BG_MAIN)
-    top_frame.pack()
+    top_frame.pack(fill="x")
 
     mid_frame = tk.Frame(root, bg=BG_MAIN)
-    mid_frame.pack()
+    mid_frame.pack(fill="both", expand=True)
 
-    bot_frame = tk.Frame(root, bg=BG_MAIN)
-    bot_frame.pack()
+    bot_frame = tk.Frame(root, bg=BG_MAIN, height=120)
+    bot_frame.pack(fill="x", side="bottom")
+    bot_frame.pack_propagate(False)
 
     #helper for frames / state changer
-    def clear_frame(frame):
-        for widget in frame.winfo_children():
-            widget.destroy()
-    
     def clear_all_frames():
         for frame in (top_frame, mid_frame, bot_frame):
             for widget in frame.winfo_children():
@@ -82,6 +82,10 @@ def start_gui():
 
     def edit_job():
         nonlocal state, jobs, job_listbox, selected_job, edit_job_id
+
+        if not job_listbox.curselection():
+            return
+
         selected_index = job_listbox.curselection()[0]
         selected_job = jobs[selected_index]
         edit_job_id = selected_job[0]
@@ -116,46 +120,52 @@ def start_gui():
             notes_entry.get(),
             link_entry.get()
         )
-    
+
         update_db(edit_job_id, job)
         clear_entry()
-    
+
     #state functions
     def render_main():
         title_label = tk.Label(top_frame, bg=BG_MAIN, fg=FG_TEXT, text="Job Tracker", font=TITLE_FONT)
         title_label.pack(pady=20)
 
         button_frame = tk.Frame(mid_frame, bg=BG_MAIN)
-        button_frame.pack(pady=40)
+        button_frame.pack(pady=20)
 
         tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Add Job", font=BUTTON_FONT, width=15, command=add_job).grid(row=0, column=0, padx=10, pady=10)
-        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT,  text="View Jobs", font=BUTTON_FONT, width=15, command=view_jobs).grid(row=0, column=1, padx=10, pady=10)
-        tk.Button(bot_frame, bg=BG_ACTIVE, fg=FG_TEXT, text="Clear DB", font=BUTTON_FONT, width=15, command=clear_db).grid(row=1, column=1, columnspan=2, pady=100)
-        tk.Button(bot_frame, bg=BG_ACTIVE, fg=FG_TEXT, text="Exit", font=BUTTON_FONT, width=15, command=root.destroy).grid(row=2, column=0, columnspan=2, pady=100)
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="View Jobs", font=BUTTON_FONT, width=15, command=view_jobs).grid(row=1, column=0, padx=10, pady=10)
+
+        tk.Button(bot_frame, bg=BG_ACTIVE, fg=FG_TEXT, text="Clear DB", font=BUTTON_FONT, width=10, command=clear_db).place(relx=1.0, rely=1.0, x=-15, y=-15, anchor="se")
 
     def render_add_job():
         nonlocal company_entry, title_entry, status_var, status_entry, date_applied_entry, location_entry, notes_entry, link_entry
 
         title_label = tk.Label(top_frame, bg=BG_MAIN, fg=FG_TEXT, text="Add Job Application", font=TITLE_FONT)
         title_label.pack(pady=20)
-    
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Company:", font=BUTTON_FONT, width=15).grid(row=0, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Job Title:", font=BUTTON_FONT, width=15).grid(row=1, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Status:", font=BUTTON_FONT, width=15).grid(row=2, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Date Applied:", font=BUTTON_FONT, width=15).grid(row=3, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Location:", font=BUTTON_FONT, width=15).grid(row=4, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Notes:", font=BUTTON_FONT, width=15).grid(row=5, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Link:", font=BUTTON_FONT, width=15).grid(row=6, column=0, padx=10, pady=10)
-        
-        company_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        title_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+
+        form_frame = tk.Frame(mid_frame, bg=BG_MAIN)
+        form_frame.pack(pady=20)
+
+        button_frame = tk.Frame(mid_frame, bg=BG_MAIN)
+        button_frame.pack(pady=20)
+
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Company:", font=BUTTON_FONT, width=15).grid(row=0, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Job Title:", font=BUTTON_FONT, width=15).grid(row=1, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Status:", font=BUTTON_FONT, width=15).grid(row=2, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Date Applied:", font=BUTTON_FONT, width=15).grid(row=3, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Location:", font=BUTTON_FONT, width=15).grid(row=4, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Notes:", font=BUTTON_FONT, width=15).grid(row=5, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Link:", font=BUTTON_FONT, width=15).grid(row=6, column=0, padx=10, pady=10)
+
+        company_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        title_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
         status_var = tk.StringVar(value="Applied")
-        status_entry = tk.OptionMenu(mid_frame, status_var, "Applied", "Interview", "Rejected", "Offer")
+        status_entry = tk.OptionMenu(form_frame, status_var, "Applied", "Interview", "Rejected", "Offer")
         status_entry.config(bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=12)
-        date_applied_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        location_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        notes_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        link_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        date_applied_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        location_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        notes_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        link_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
 
         company_entry.grid(row=0, column=2, padx=10, pady=10)
         title_entry.grid(row=1, column=2, padx=10, pady=10)
@@ -165,8 +175,8 @@ def start_gui():
         notes_entry.grid(row=5, column=2, padx=10, pady=10)
         link_entry.grid(row=6, column=2, padx=10, pady=10)
 
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Save", font=BUTTON_FONT, width=15, command=on_save_job).grid(row=0, column=0, columnspan=1, pady=10)
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).grid(row=1, column=0, columnspan=1, pady=10)
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Save", font=BUTTON_FONT, width=15, command=on_save_job).grid(row=0, column=0, pady=10)
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).grid(row=1, column=0, pady=10)
 
     def render_view_job():
         nonlocal jobs, job_listbox
@@ -180,8 +190,11 @@ def start_gui():
             job_listbox.insert(tk.END, job)
         job_listbox.pack()
 
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Edit", font=BUTTON_FONT, width=15, command=edit_job).grid(row=0, column=0, columnspan=1, pady=10)
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).grid(row=1, column=0, columnspan=1, pady=10)
+        button_frame = tk.Frame(mid_frame, bg=BG_MAIN)
+        button_frame.pack(pady=20)
+
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Edit", font=BUTTON_FONT, width=15, command=edit_job).pack()
+        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).place(relx=0.0, rely=1.0, x=15, y=-15, anchor="sw")
 
     def render_edit_job():
         nonlocal company_entry, title_entry, status_var, status_entry, date_applied_entry, location_entry, notes_entry, link_entry, selected_job
@@ -189,23 +202,29 @@ def start_gui():
         title_label = tk.Label(top_frame, bg=BG_MAIN, fg=FG_TEXT, text="Edit Job Application", font=TITLE_FONT)
         title_label.pack(pady=20)
 
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Company:", font=BUTTON_FONT, width=15).grid(row=0, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Job Title:", font=BUTTON_FONT, width=15).grid(row=1, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Status:", font=BUTTON_FONT, width=15).grid(row=2, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Date Applied:", font=BUTTON_FONT, width=15).grid(row=3, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Location:", font=BUTTON_FONT, width=15).grid(row=4, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Notes:", font=BUTTON_FONT, width=15).grid(row=5, column=0, padx=10, pady=10)
-        tk.Label(mid_frame, bg=BG_MAIN, fg=FG_TEXT, text="Link:", font=BUTTON_FONT, width=15).grid(row=6, column=0, padx=10, pady=10)
+        form_frame = tk.Frame(mid_frame, bg=BG_MAIN)
+        form_frame.pack(pady=20)
 
-        company_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        title_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        button_frame = tk.Frame(mid_frame, bg=BG_MAIN)
+        button_frame.pack(pady=20)
+
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Company:", font=BUTTON_FONT, width=15).grid(row=0, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Job Title:", font=BUTTON_FONT, width=15).grid(row=1, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Status:", font=BUTTON_FONT, width=15).grid(row=2, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Date Applied:", font=BUTTON_FONT, width=15).grid(row=3, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Location:", font=BUTTON_FONT, width=15).grid(row=4, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Notes:", font=BUTTON_FONT, width=15).grid(row=5, column=0, padx=10, pady=10)
+        tk.Label(form_frame, bg=BG_MAIN, fg=FG_TEXT, text="Link:", font=BUTTON_FONT, width=15).grid(row=6, column=0, padx=10, pady=10)
+
+        company_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        title_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
         status_var = tk.StringVar(value="Applied")
-        status_entry = tk.OptionMenu(mid_frame, status_var, "Applied", "Interview", "Rejected", "Offer")
+        status_entry = tk.OptionMenu(form_frame, status_var, "Applied", "Interview", "Rejected", "Offer")
         status_entry.config(bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=12)
-        date_applied_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        location_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        notes_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
-        link_entry = tk.Entry(mid_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        date_applied_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        location_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        notes_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
+        link_entry = tk.Entry(form_frame, bg=BG_WIDGET, fg=FG_TEXT, font=BUTTON_FONT, width=15)
 
         company_entry.grid(row=0, column=2, padx=10, pady=10)
         title_entry.grid(row=1, column=2, padx=10, pady=10)
@@ -223,9 +242,9 @@ def start_gui():
         notes_entry.insert(0, selected_job[6])
         link_entry.insert(0, selected_job[7])
 
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Save", font=BUTTON_FONT, width=15, command=on_save_edit_job).grid(row=0, column=0, columnspan=1, pady=10)
-        tk.Button(bot_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).grid(row=1, column=0, columnspan=1, pady=10)
-        
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Save", font=BUTTON_FONT, width=15, command=on_save_edit_job).grid(row=0, column=0, pady=10)
+        tk.Button(button_frame, bg=BG_WIDGET, fg=FG_TEXT, text="Back", font=BUTTON_FONT, width=15, command=main_menu).grid(row=1, column=0, pady=10)
+
     #state if chain
     if state == "main":
         render_main()
